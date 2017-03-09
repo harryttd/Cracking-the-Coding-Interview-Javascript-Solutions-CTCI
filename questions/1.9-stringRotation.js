e6c16aff4b145b6b4b33aea2d04846556fa93c89
Aryeh Harris <harryttd@gmail.com>
2017-03-09 17:47:49 -0500

    Add stringRotation solution

---
+++ questions/1.9-stringRotation.js
@@ -0,0 +1,8 @@
+export function stringRotation(str1, str2) {
+  if (!str1 || !str1.length || !str2 || !str2.length) throw Error('invalid input');
+  if (str1.length !== str2.length) return false;
+
+  return (str2 + str2).indexOf(str1) > -1;
+}
+
+console.log(stringRotation('waterbottle', 'erbottlewat'));
