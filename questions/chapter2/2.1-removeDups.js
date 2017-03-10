e3e6ae42834adb56ad04cd88c5b329aa35acaa10
Aryeh Harris <harryttd@gmail.com>
2017-03-09 22:20:00 -0500

    Add remove dupes from linked list in O(N) time O(N) space solution and also O(N^2) time O(1) space solution.

--- questions/chapter2/2.1-removeDups.js
+++ questions/chapter2/2.1-removeDups.js
@@ -1,6 +1,7 @@
 // USING A SET
-export function removeDups(list) {
-  if (!list) return list;
+// O(N) RUNTIME - O(N) SPACE
+export function removeDups1(list) {
+  if (!list || !list.next) return list;
   const set = new Set();
   set.add(list.value);
   while (list.next) {
@@ -11,5 +12,20 @@ export function removeDups(list) {
       list = list.next;
     }
   }
-  return list;
+}
+
+// O(N^2) RUNTIME
+export function removeDups2(list) {
+  if (!list || !list.next) return list;
+  while (list) {
+    let head = list;
+    while (head.next) {
+      if (head.next.value === list.value) {
+        head.next = head.next.next;
+      } else {
+        head = head.next;
+      }
+    }
+    list = list.next;
+  }
 }
