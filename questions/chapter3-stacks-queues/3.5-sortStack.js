12c01194bd238a91172ac0a7e3a065f61f81a53c
Aryeh Harris <harryttd@gmail.com>
2017-03-24 18:22:42 -0400

    Add my own and book solution to sort stack.

---
+++ questions/chapter3-stacks-queues/3.5-sortStack.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const isEmpty = (stack) => !stack.length;
+const peek = (stack) => stack[stack.length - 1];
+
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+// MY SOLUTION
+const sort = (stack, value) => {
+  let index = 0;
+  while (value < stack[index]) index++;
+  stack.splice(index, 0, value);
+};
+
+export const sortStack1 = (stack) => {
+  const newStack = [stack.pop()];
+  while (!isEmpty(stack)) sort(newStack, stack.pop());
+  return newStack;
+};
+
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+// BOOK SOLUTION
+// MOVES NUMBERS BACK AND FORTH BETWEEN STACK AND TEMP TO SORT
+export const sortStack2 = (stack) => {
+  const temp = [];
+  while (!isEmpty(stack)) {
+    const popped = stack.pop();
+    while (!isEmpty(temp) && peek(temp) > popped) {
+      stack.push(temp.pop());
+    }
+    temp.push(popped);
+  }
+
+  while (!isEmpty(temp)) stack.push(temp.pop());
+
+  return stack;
+};
