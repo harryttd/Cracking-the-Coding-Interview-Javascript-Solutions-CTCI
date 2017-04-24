5d257979edf1fbb082557c22382f9dbf280e9df8
Aryeh Harris <harryttd@gmail.com>
2017-04-24 00:43:34 -0400

    Update palindrome.

--- misc/palindrome.js
+++ misc/palindrome.js
@@ -1,21 +1,35 @@
 'use strict';
 
-export function palindrome(str) {
+// O(N/2) TIME --- O(1) SPACE
+
+export function palindromeWhileLoop(str) {
   if (typeof str !== 'string') throw Error('invalid input');
 
-  const length = str.length,
-        half = length / 2,
-        even = length % 2 === 0;
+  str = str.toLowerCase();
 
-  let head = 0, tail = length - 1;
+  let head = 0, tail = str.length - 1;
 
-  while (even ? head < half : head !== tail) {
+  while (head++ < tail--) {
     if (str[head] !== str[tail]) return false;
-    head++;
-    tail--;
   }
 
   return true;
 }
 
-export const palindromeReverseMethod = (str) => str === str.split``.reverse().join``;
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+export function palindromeForLoop(str) {
+  if (typeof str !== 'string') throw Error('invalid input');
+
+  str = str.toLowerCase();
+
+  for (let i = 0, x = str.length - 1; i < x; i++, x--) {
+    if (str[i] !== str[x]) return false;
+  }
+
+  return true;
+}
+
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+export const palindromeReverseMethod = (str) => str.toLowerCase() === str.split``.reverse().join``.toLowerCase();
--- misc/palindrome.spec.js
+++ misc/palindrome.spec.js
@@ -4,15 +4,20 @@ import * as funcs from './palindrome';
 for (let key in funcs) {
   const func = funcs[key];
 
-  describe('MISC Palindrome: ' + key, function() {
+  describe('MISC - Palindrome: ' + key, function() {
 
-    if (func.name === 'palindrome') {
+    if (func.name) {
       it('throws error if input is not a string', function() {
         expect(() => func(null)).to.throw('invalid input');
+        expect(() => func(undefined)).to.throw('invalid input');
       });
     }
 
-    it('returns true/false for odd length string', function() {
+    it('return true on empty string', function() {
+      expect(func('')).to.be.true;
+    });
+
+    it('returns correct true/false for odd length string', function() {
       expect(func('racecar')).to.be.true;
       expect(func('civic')).to.be.true;
 
@@ -20,12 +25,17 @@ for (let key in funcs) {
       expect(func('civc')).to.be.false;
     });
 
-    it('returns true/false for even length string', function() {
+    it('returns correct true/false for even length string', function() {
       expect(func('noon')).to.be.true;
       expect(func('redder')).to.be.true;
 
       expect(func('noono')).to.be.false;
       expect(func('redderx')).to.be.false;
+    });
+
+    it('returns true for mixed case string', function() {
+      expect(func('RaceCar')).to.be.true;
+      expect(func('Civic')).to.be.true;
     });
 
   });
