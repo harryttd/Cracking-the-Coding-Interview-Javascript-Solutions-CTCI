ebdc09627ee3eef2f41d756ef8602d2dfacc75fc
Aryeh Harris <harryttd@gmail.com>
2017-04-26 22:42:08 -0400

    Add book's second solution to firstCommonAncestor.

--- questions-specs/chapter4-trees-graphs/4.08-firstCommonAncestor.js
+++ questions-specs/chapter4-trees-graphs/4.08-firstCommonAncestor.js
@@ -1,8 +1,11 @@
 'use strict';
 
+// USING PARENT REFERENCE
+// O(DEPTH OF DEEPER NODE) TIME --- O(1) SPACE
+
 const getDepth = (node, depth = 0) => node.parent ? getDepth(node.parent, depth + 1) : depth;
 
-export function firstCommonAncestor(node1, node2) {
+export function firstCommonAncestor1(node1, node2) {
   if (!node1 || !node2) throw Error('invalid node(s)');
 
   const depth1 = getDepth(node1), depth2 = getDepth(node2);
@@ -19,4 +22,39 @@ export function firstCommonAncestor(node1, node2) {
     deeper = deeper.parent;
   }
 
+}
+
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+// USING PARENT REFERENCE
+// O(SUBTREE SIZE OF FIRST COMMON ANCESTOR) TIME --- O(1) SPACE
+// WORST CASE O(N) TIME
+
+export function firstCommonAncestor2(node1, node2, tree) {
+  if (!node1 || !node2) throw Error('invalid node(s)');
+
+  // if (!covers(tree, node1) || !covers(tree, node2)) return null;
+  if (covers(node1, node2)) return node1.value;
+  if (covers(node2, node1)) return node2.value;
+
+  let sibling = getSibling(node1), parent = node1.parent;
+
+  while (!covers(sibling, node2)) {
+    sibling = getSibling(parent);
+    parent = parent.parent;
+  }
+
+  return parent.value;
+}
+
+function covers(tree, node) {
+  if (!tree) return false;
+  if (tree === node) return true;
+  return covers(tree.left, node) || covers(tree.right, node);
+}
+
+function getSibling(node) {
+  if (!node || !node.parent) return null;
+  const parent = node.parent;
+  return parent.left === node ? parent.right : parent.left;
 }
--- questions-specs/chapter4-trees-graphs/4.08-firstCommonAncestor.spec.js
+++ questions-specs/chapter4-trees-graphs/4.08-firstCommonAncestor.spec.js
@@ -5,7 +5,7 @@ import * as funcs from './4.08-firstCommonAncestor';
 for (let key in funcs) {
   let func = funcs[key];
 
-  describe('ch4-q08: ' + key, function() {
+  describe.only('ch4-q08: ' + key, function() {
 
     beforeEach(function() {
       this.tree = new Tree();
@@ -18,35 +18,35 @@ for (let key in funcs) {
       expect(() => func(null, this.tree.root)).to.throw('invalid node(s)');
     });
 
-    it('returns right value for simple 3 node balanced tree', function() {
+    it('returns correct value for simple 3 node balanced tree', function() {
       [10, 9, 11].forEach(v => this.tree.add(v));
-      expect(func(this.tree.root.left, this.tree.root.right)).to.equal(10);
-      expect(func(this.tree.root, this.tree.root.right)).to.equal(10);
-      expect(func(this.tree.root.left, this.tree.root)).to.equal(10);
+      expect(func(this.tree.root.left, this.tree.root.right, this.tree)).to.equal(10);
+      expect(func(this.tree.root, this.tree.root.right, this.tree)).to.equal(10);
+      expect(func(this.tree.root.left, this.tree.root, this.tree)).to.equal(10);
     });
 
     it('returns correct values for larger balanced tree', function() {
       [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15].forEach(v => this.tree.add(v));
 
-      expect(func(this.tree.root.left.left.left, this.tree.root.left.left.left)).to.equal(1);
-      expect(func(this.tree.root.left.left.left, this.tree.root.left.left.right)).to.equal(2);
-      expect(func(this.tree.root.left.left.right, this.tree.root.left.left.left)).to.equal(2);
+      expect(func(this.tree.root.left.left.left, this.tree.root.left.left.left, this.tree)).to.equal(1);
+      expect(func(this.tree.root.left.left.left, this.tree.root.left.left.right, this.tree)).to.equal(2);
+      expect(func(this.tree.root.left.left.right, this.tree.root.left.left.left, this.tree)).to.equal(2);
 
-      expect(func(this.tree.root.left, this.tree.root.left)).to.equal(4);
-      expect(func(this.tree.root.left.left.right, this.tree.root.left)).to.equal(4);
-      expect(func(this.tree.root.left.left.right, this.tree.root.left.right)).to.equal(4);
-      expect(func(this.tree.root.left.left.left, this.tree.root.left.right.left)).to.equal(4);
-      expect(func(this.tree.root.left.left.left, this.tree.root.left.right.right)).to.equal(4);
-      expect(func(this.tree.root.left.left.right, this.tree.root.left.right.left)).to.equal(4);
-      expect(func(this.tree.root.left.left.right, this.tree.root.left.right.right)).to.equal(4);
+      expect(func(this.tree.root.left, this.tree.root.left, this.tree)).to.equal(4);
+      expect(func(this.tree.root.left.left.right, this.tree.root.left, this.tree)).to.equal(4);
+      expect(func(this.tree.root.left.left.right, this.tree.root.left.right, this.tree)).to.equal(4);
+      expect(func(this.tree.root.left.left.left, this.tree.root.left.right.left, this.tree)).to.equal(4);
+      expect(func(this.tree.root.left.left.left, this.tree.root.left.right.right, this.tree)).to.equal(4);
+      expect(func(this.tree.root.left.left.right, this.tree.root.left.right.left, this.tree)).to.equal(4);
+      expect(func(this.tree.root.left.left.right, this.tree.root.left.right.right, this.tree)).to.equal(4);
 
-      expect(func(this.tree.root.left.left.right, this.tree.root)).to.equal(8);
-      expect(func(this.tree.root.left.left.right, this.tree.root.right)).to.equal(8);
-      expect(func(this.tree.root.left.left.right, this.tree.root.right.right)).to.equal(8);
-      expect(func(this.tree.root.left.left.right, this.tree.root.right.left.left)).to.equal(8);
-      expect(func(this.tree.root.left.left.right, this.tree.root.right.left.right)).to.equal(8);
-      expect(func(this.tree.root.left.left.right, this.tree.root.right.right.left)).to.equal(8);
-      expect(func(this.tree.root.left.left.right, this.tree.root.right.right.right)).to.equal(8);
+      expect(func(this.tree.root.left.left.right, this.tree.root, this.tree)).to.equal(8);
+      expect(func(this.tree.root.left.left.right, this.tree.root.right, this.tree)).to.equal(8);
+      expect(func(this.tree.root.left.left.right, this.tree.root.right.right, this.tree)).to.equal(8);
+      expect(func(this.tree.root.left.left.right, this.tree.root.right.left.left, this.tree)).to.equal(8);
+      expect(func(this.tree.root.left.left.right, this.tree.root.right.left.right, this.tree)).to.equal(8);
+      expect(func(this.tree.root.left.left.right, this.tree.root.right.right.left, this.tree)).to.equal(8);
+      expect(func(this.tree.root.left.left.right, this.tree.root.right.right.right, this.tree)).to.equal(8);
     });
 
   });
