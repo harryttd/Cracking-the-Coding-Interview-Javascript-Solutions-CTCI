be55336a257ba874179f053170f837352077b5dc
Aryeh Harris <harryttd@gmail.com>
2017-04-27 16:40:54 -0400

    Add 3rd and 4th book solutions to firstCommonAncestor.

--- questions-specs/chapter4-trees-graphs/4.08-firstCommonAncestor.js
+++ questions-specs/chapter4-trees-graphs/4.08-firstCommonAncestor.js
@@ -47,14 +47,78 @@ export function firstCommonAncestor2(node1, node2, tree) {
   return parent.value;
 }
 
+function getSibling(node) {
+  if (!node || !node.parent) return null;
+  const parent = node.parent;
+  return parent.left === node ? parent.right : parent.left;
+}
+
 function covers(tree, node) {
   if (!tree) return false;
   if (tree === node) return true;
   return covers(tree.left, node) || covers(tree.right, node);
 }
 
-function getSibling(node) {
-  if (!node || !node.parent) return null;
-  const parent = node.parent;
-  return parent.left === node ? parent.right : parent.left;
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+// (NOT OPTIMIZED) NOT USING PARENT REFERENCE
+// O(N) TIME (covers() is called first on 2n nodes, then 2n/2, 2n/4, ...)
+
+export function firstCommonAncestor3(node1, node2, tree) {
+  if (!node1 || !node2) throw Error('invalid node(s)');
+  else if (!covers(tree, node1) || !covers(tree, node2)) return null;
+
+  return findAncestor(node1, node2, tree).value;
+}
+
+function findAncestor(node1, node2, tree) {
+  if (tree === node1 || tree === node2) return tree;
+
+  const node1onLeft = covers(tree.left, node1), node2onLeft = covers(tree.left, node2);
+
+  if (node1onLeft !== node2onLeft) return tree;
+
+  const childSide = node1onLeft ? tree.left : tree.right;
+  return findAncestor(node1, node2, childSide);
+}
+
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+// (OPTIMIZED) NOT USING PARENT REFERENCE
+
+class Result {
+  constructor(node, isAncestor) {
+    this.node = node;
+    this.isAncestor = isAncestor;
+  }
+}
+
+export function firstCommonAncestor4(node1, node2, tree) {
+  if (!node1 || !node2) throw Error('invalid node(s)');
+  const result = findCommonAncestor(node1, node2, tree);
+  return result.isAncestor ? result.node.value : null;
+}
+
+function findCommonAncestor(node1, node2, tree) {
+  if (!tree) return new Result(null, false);
+
+  if (tree === node1 && tree === node2) return new Result(tree, true);
+
+  const nodeLeft = findCommonAncestor(node1, node2, tree.left);
+  if (nodeLeft.isAncestor) return nodeLeft;
+
+  const nodeRight = findCommonAncestor(node1, node2, tree.right);
+  if (nodeRight.isAncestor) return nodeRight;
+
+  if (nodeLeft.node && nodeRight.node) {
+    return new Result(tree, true);
+  }
+  else if (tree === node1 || tree === node2) {
+    const isAncestor = nodeLeft.node || nodeRight.node;
+    return new Result(tree, isAncestor);
+  }
+  else {
+    return new Result(nodeLeft.node ? nodeLeft.node : nodeRight.node, false);
+  }
+
 }
