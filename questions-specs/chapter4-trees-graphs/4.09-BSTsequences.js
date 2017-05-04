111d5947940b17a48074a4af93d66780b16d98c1
Aryeh Harris <harryttd@gmail.com>
2017-05-03 23:18:51 -0400

    Add spec for BST Sequences to test for even array length. Update code.

--- questions-specs/chapter4-trees-graphs/4.09-BSTsequences.js
+++ questions-specs/chapter4-trees-graphs/4.09-BSTsequences.js
@@ -1,56 +1,50 @@
 'use strict';
 
-function weave(leftArray, rightArray, prefix = [], result = []) {
-  if (!leftArray.length || !rightArray.length) {
-    result.push([...prefix, ...leftArray, ...rightArray]);
-    console.log(result);
-    return result;
-  }
-
-  const h1 = leftArray.shift();
-  prefix.push(h1);
-  weave(leftArray, rightArray, prefix, result);
-  leftArray.unshift(h1);
-  prefix.pop();
-
-  const h2 = rightArray.shift();
-  prefix.push(h2);
-  weave(leftArray, rightArray, prefix, result);
-  rightArray.unshift(h2);
-  prefix.pop();
-
-  return result;
-}
-
 export function BSTsequences(tree) {
   if (!tree) return [];
 
-  if (!tree.left && !tree.right) {
-    return [[tree.value]];
-  }
+  if (!tree.left && !tree.right) return [[tree.value]];
 
-  const bstResult = [];
-  const left = BSTsequences(tree.left);
-  const right = BSTsequences(tree.right);
-  // console.log("LEFT", left, tree.value);
-  // console.log("RIGHT", right, tree.value);
+  const bstResult = [],
+        left = BSTsequences(tree.left),
+        right = BSTsequences(tree.right);
+
   if (left.length && right.length) {
     for (const leftSequence of left) {
       for (const rightSequence of right) {
-        const weaved = weave(leftSequence, rightSequence);
+        const weaved = weaveArrayPerms(leftSequence, rightSequence);
         for (const sequence of weaved) {
-          // console.log(sequence);
           bstResult.push([tree.value, ...sequence]);
-          // console.log(bstResult);
         }
       }
     }
+  } else {
+    const remaining = [...left, ...right];
+    for (const sequence of remaining) {
+      bstResult.push([tree.value, ...sequence]);
+    }
   }
-  // else {
-    // console.log(left, right);
-    // const remaining = [...left, ...right];
-    // bstResult.push([[tree.value, ...remaining]]);
-  // }
-  // console.log("BST RESULT", bstResult);
+
   return bstResult;
+}
+
+function weaveArrayPerms(leftArray, rightArray, prefix = [], result = []) {
+  if (leftArray.length && rightArray.length) {
+    const p1 = leftArray.shift();
+    prefix.push(p1);
+    weaveArrayPerms(leftArray, rightArray, prefix, result);
+    leftArray.unshift(p1);
+    prefix.pop();
+
+    const p2 = rightArray.shift();
+    prefix.push(p2);
+    weaveArrayPerms(leftArray, rightArray, prefix, result);
+    rightArray.unshift(p2);
+    prefix.pop();
+  }
+  else {
+    result.push([...prefix, ...leftArray, ...rightArray]);
+  }
+
+  return result;
 }
