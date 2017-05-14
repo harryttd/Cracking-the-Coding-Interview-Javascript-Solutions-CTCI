dd24e95ca9f9311d69543d25a3fd6d62f7ed3a02
Aryeh Harris <harryttd@gmail.com>
2017-05-14 00:39:04 -0400

    Completed chapter 4! Add second solution to pathsWithSumX.

--- questions-specs/chapter4-trees-graphs/4.12-pathsWithSumX.js
+++ questions-specs/chapter4-trees-graphs/4.12-pathsWithSumX.js
@@ -1,25 +1,55 @@
 'use strict';
 
-export function treePathsWithSumX(tree, value) {
+export function treePathsWithSumX_1(tree, value) {
   if (!tree || !tree.root) throw Error('invalid tree');
-  return countPaths(tree.root, value);
+  return countPaths1(tree.root, value);
 }
 
-function countPaths(tree, value, path = [], paths = 0) {
+function countPaths1(tree, value, path = [], paths = 0) {
   if (tree) {
     path.push(tree.value);
 
     let sum = 0,
-        pathIndex = path.length - 1;
+        pathLength = path.length;
 
-    while (!!~pathIndex) {
-      sum += path[pathIndex--];
+    while (!!~pathLength--) {
+      sum += path[pathLength];
       if (sum === value) paths++;
     }
 
-    paths += countPaths(tree.left, value, path) + countPaths(tree.right, value, path);
+    paths += countPaths1(tree.left, value, path) + countPaths1(tree.right, value, path);
     path.pop();
   }
+
+  return paths;
+}
+
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+export function treePathsWithSumX_2(tree, value) {
+  if (!tree || !tree.root) throw Error('invalid tree');
+  return countPaths2(tree.root, value);
+}
+
+function countPaths2(tree, value) {
+  if (!tree) return 0;
+
+  const pathsFromRoot = countFromNode(tree, value),
+        leftPaths = countPaths2(tree.left, value),
+        rightPaths = countPaths2(tree.right, value);
+
+  return pathsFromRoot + leftPaths + rightPaths;
+}
+
+function countFromNode(tree, value, sum = 0) {
+  if (!tree) return 0;
+
+  sum += tree.value;
+
+  let paths = 0;
+  if (sum === value) paths++;
+
+  paths += countFromNode(tree.left, value, sum) + countFromNode(tree.right, value, sum);
 
   return paths;
 }
