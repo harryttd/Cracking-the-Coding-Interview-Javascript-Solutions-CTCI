2c2406aa0b15a8e6a5df190e07557579f56e1060
Aryeh Harris <harryttd@gmail.com>
2017-05-17 18:03:19 -0400

    Working on chapter 16 moderate problems. Add solutions to numberSwapper.

---
+++ questions-specs/chapter16-moderate/16.01-numberSwapper.js
@@ -0,0 +1,21 @@
+'use strict';
+
+console.log('Number Swap Addition Subtraction');
+function numberSwap1(a, b) {
+  a = a - b;
+  b = a + b;
+  a = b - a;
+  return `a: ${a}, b: ${b}`;
+}
+console.log(numberSwap1(3, 15));
+console.log(numberSwap1(-3, 7));
+
+function numberSwap2(a, b) {
+  a = a ^ b;
+  b = a ^ b;
+  a = a ^ b;
+  return `a: ${a}, b: ${b}`;
+}
+console.log('\nNumber Swap Bitwise');
+console.log(numberSwap2(-3, 7));
+console.log(numberSwap2(15, 4));
