e8a768cb5191261d51adb12ab2a6df3d0be6bb04
Aryeh Harris <harryttd@gmail.com>
2017-05-24 15:56:11 -0400

    Add second solution to powerSet.

--- questions-specs/chapter08-recursion-and-dynamic-programming/8.04-powerSet.js
+++ questions-specs/chapter08-recursion-and-dynamic-programming/8.04-powerSet.js
@@ -4,11 +4,11 @@
 // 2^n SUBSETS --- EACH ELEMENT WILL BE IN 2^n-1 (half) SUBSETS
 // TOTAL NUMBER OF ELEMENTS IN SUBSETS IS N*2^n-1
 
-function powerSet(set, index = set.length - 1) {
+function powerSetRecursive(set, index = set.length - 1) {
   let allSubsets;
 
   if (!!~index) {
-    allSubsets = powerSet(set, index - 1);
+    allSubsets = powerSetRecursive(set, index - 1);
 
     const item = set[index], { length } = allSubsets;
     for (let i = 0; i < length; i++) {
@@ -21,5 +21,31 @@ function powerSet(set, index = set.length - 1) {
 
   return allSubsets;
 }
-console.log(powerSet(['a', 'b', 'c']));
-console.log(powerSet([1, 2, 3, 4]));
+console.log(powerSetRecursive(['a', 'b', 'c']));
+console.log(powerSetRecursive([1, 2, 3, 4]));
+
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+function powerSetIterative(set) {
+  let allSubsets = [];
+  const max = 1 << set.length;
+
+  for (let k = 0; k < max; k++) {
+    const subset = convertNumToSet(k, set);
+    allSubsets.push(subset);
+  }
+  return allSubsets;
+}
+
+function convertNumToSet(x, set) {
+  const subset = [];
+  let index = 0;
+
+  for (let k = x; k > 0; k >>= 1) {
+    if ((k & 1) === 1) subset.push(set[index]);
+    index++;
+  }
+  return subset;
+}
+console.log(powerSetIterative(['x', 'y', 'z']));
+console.log(powerSetIterative([5, 6, 7, 8]));
