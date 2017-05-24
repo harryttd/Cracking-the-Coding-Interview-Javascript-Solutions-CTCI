1c0d14ce537739cf13bc1f0a46097cdbc2893550
Aryeh Harris <harryttd@gmail.com>
2017-05-24 15:35:56 -0400

    Update first powerSet solution.

--- questions-specs/chapter08-recursion-and-dynamic-programming/8.04-powerSet.js
+++ questions-specs/chapter08-recursion-and-dynamic-programming/8.04-powerSet.js
@@ -1,16 +1,19 @@
 'use strict';
 
+// O(N2^n) TIME AND SPACE
+// 2^n SUBSETS --- EACH ELEMENT WILL BE IN 2^n-1 (half) SUBSETS
+// TOTAL NUMBER OF ELEMENTS IN SUBSETS IS N*2^n-1
+
 function powerSet(set, index = set.length - 1) {
   let allSubsets;
 
   if (!!~index) {
     allSubsets = powerSet(set, index - 1);
-    const item = set[index], moreSubsets = [];
 
-    for (const subset of allSubsets) {
-      moreSubsets.push([...subset, item]);
+    const item = set[index], { length } = allSubsets;
+    for (let i = 0; i < length; i++) {
+      allSubsets.push([...allSubsets[i], item]);
     }
-    allSubsets.push(...moreSubsets);
   }
   else {
     allSubsets = [[]];
