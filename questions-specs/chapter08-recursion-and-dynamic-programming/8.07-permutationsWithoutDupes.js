70119bd7d8a47d058efe71f7a57549a3fadad6d2
Aryeh Harris <harryttd@gmail.com>
2017-05-29 15:01:09 -0400

    Update 8.07.

--- questions-specs/chapter08-recursion-and-dynamic-programming/8.07-permutationsWithoutDupes.js
+++ questions-specs/chapter08-recursion-and-dynamic-programming/8.07-permutationsWithoutDupes.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// Solutions for permutations without duplicate characters in original string.
+
+// UPPER BOUND OF FUNCTION CALLS: O(N * !N)
+// RUNTIME WILL NOT EXCEED: O(Nˆ2 * !N)
+
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+// Pushes every prefix down the stack
 function permutationsNoDupes1(str, prefix = '', result = []) {
   const { length } = str;
   if (!length) return result.push(prefix);
@@ -14,6 +22,9 @@ console.log(permutationsNoDupes1('abc'));
 
 // |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
 
+// Inserts every letter of string into every position of all permutations
+
+// Passes permutations back up the stack
 function permutationsNoDupes2(str, result = []) {
   if (!str) {
     result.push(str);
@@ -26,6 +37,7 @@ function permutationsNoDupes2(str, result = []) {
   for (const word of words) {
     const { length } = word;
     for (let i = 0; i <= length; i++) {
+      // Insert prefix into every position
       const perm = word.slice(0, i) + prefix + word.slice(i);
       result.push(perm);
     }
@@ -37,6 +49,9 @@ console.log(permutationsNoDupes2('abc'));
 
 // |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
 
+// Uses every letter of string as the first char and appends every permutation to it
+
+// Passes permutations back up the stack
 function permutationsNoDupes3(str) {
   const result = [], { length } = str;
 
@@ -48,10 +63,10 @@ function permutationsNoDupes3(str) {
   for (let i = 0; i < length; i++) {
     // Remove char at i and find perms of remaining
     const remaining = str.slice(0, i) + str.slice(i + 1);
-    const partials = permutationsNoDupes3(remaining);
+    const perms = permutationsNoDupes3(remaining);
 
-    for (const word of partials) {
-      result.push(str[i] + word);
+    for (const perm of perms) {
+      result.push(str[i] + perm);
     }
   }
 
