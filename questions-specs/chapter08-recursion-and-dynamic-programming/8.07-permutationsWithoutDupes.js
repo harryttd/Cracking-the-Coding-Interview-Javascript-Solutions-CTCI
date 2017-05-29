a5a4664274b23c6d6472b48ac193284ade37a828
Aryeh Harris <harryttd@gmail.com>
2017-05-28 23:24:06 -0400

    Add second solution to permutationsNoDupes.

--- questions-specs/chapter08-recursion-and-dynamic-programming/8.07-permutationsWithoutDupes.js
+++ questions-specs/chapter08-recursion-and-dynamic-programming/8.07-permutationsWithoutDupes.js
@@ -1,14 +1,38 @@
 'use strict';
 
-function permutationsNoDupes(str, prefix = '', result = []) {
+function permutationsNoDupes1(str, prefix = '', result = []) {
   const { length } = str;
   if (!length) return result.push(prefix);
 
   for (let i = 0; i < length; i++) {
-    permutationsNoDupes(str.slice(0, i) + str.slice(i + 1), prefix + str[i], result);
+    permutationsNoDupes1(str.slice(0, i) + str.slice(i + 1), prefix + str[i], result);
   }
 
   return result;
 }
+console.log(permutationsNoDupes1('abc'));
 
-console.log(permutationsNoDupes('abc'));
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
+
+function permutationsNoDupes2(str, result = []) {
+  if (!str) {
+    result.push(str);
+    return result;
+  }
+
+  const prefix = str[0],
+        words = permutationsNoDupes2(str.slice(1));
+
+  for (const word of words) {
+    const { length } = word;
+    for (let i = 0; i <= length; i++) {
+      const perm = word.slice(0, i) + prefix + word.slice(i);
+      result.push(perm);
+    }
+  }
+
+  return result;
+}
+console.log(permutationsNoDupes2('abc'));
+
+// |---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---|
