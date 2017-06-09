92124e0114134210a41b5d92a3e85591a0ed9f90
Aryeh Harris <harryttd@gmail.com>
2017-06-09 19:18:58 -0400

    Add sieve primes solution to misc.

---
+++ misc/primes-sieveOfEratosthenes.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function eratosthenes(n) {
+  // Eratosthenes algorithm to find all primes under n
+  const array = new Array(n).fill(true), upperLimit = Math.sqrt(n), output = [];
+
+  // Remove multiples of primes starting from 2, 3, 5,...
+  for (let i = 2; i <= upperLimit; i++) {
+    if (array[i]) {
+      for (let j = i * i; j < n; j += i) {
+        // console.log(j);
+        array[j] = false;
+      }
+    }
+  }
+
+  // All array[i] set to true are primes
+  for (let i = 2; i < n; i++) {
+    if (array[i]) {
+      output.push(i);
+    }
+  }
+  return output;
+}
+
+console.log(eratosthenes(1000000));
