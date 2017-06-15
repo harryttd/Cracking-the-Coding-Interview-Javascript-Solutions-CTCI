4d22c00f0de548a60a2b7a88f636084078eef213
Aryeh Harris <harryttd@gmail.com>
2017-06-14 22:10:46 -0400

    Update reverse number in misc to handle negative numbers.

--- misc/reverseNumber.js
+++ misc/reverseNumber.js
@@ -1,14 +1,17 @@
 'use strict';
 
 function reverseNum(num) {
+  const isPositive = num >= 0;
   let reversed = 0;
 
-  while (num > 0) {
-    const remainder = num % 10;
+  while (num) {
+    const remainder = Math.abs(num % 10);
     reversed = reversed * 10 + remainder;
-    num = Math.floor(num / 10);
+    num = Math.floor(Math.abs(num) / 10);
   }
 
-  return reversed;
+  return isPositive ? reversed : -reversed;
 }
+
 console.log(reverseNum(12345));
+console.log(reverseNum(-12345));
